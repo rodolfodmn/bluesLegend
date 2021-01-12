4a4e513f9b07d73cf8f2d1f92bccbb6b133d7a31
rodolfo domingos <rodolfo.dmn@gmail.com>
2021-01-12 20:46:21 -0300

    retirando defaults e criando main screen

---
+++ client/src/components/MainScreen.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+    <h1>main screen</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainScreen',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped></style>
